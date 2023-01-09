41e1a58bc9180f0da48945f1e55b13be180a02ef
JellyDev <yung5060@kakao.com>
2023-01-09 23:35:07 +0900

    58:05

---
+++ DemoApp/src/main/java/com/telusko/SqrServlet.java
@@ -0,0 +1,22 @@
+package com.telusko;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqrServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = (int) req.getAttribute("k");
+		
+		k = k * k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Result is " + k);
+	}
+
+}
