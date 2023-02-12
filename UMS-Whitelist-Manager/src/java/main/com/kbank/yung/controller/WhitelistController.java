bbab7e23ebaec870962b4e68c68fee238efc5ec9
JellyDev <yung5060@kakao.com>
2023-02-12 22:02:40 +0900

    .

--- UMS-Whitelist-Manager/src/java/main/com/kbank/yung/controller/WhitelistController.java
+++ UMS-Whitelist-Manager/src/java/main/com/kbank/yung/controller/WhitelistController.java
@@ -37,11 +37,11 @@ public class WhitelistController {
 		int total = service.countWhiteMembers();
 		if (nowPage == null && cntPerPage == null) {
 			nowPage = "1";
-			cntPerPage = "5";
+			cntPerPage = "20";
 		} else if (nowPage == null) {
 			nowPage = "1";
 		} else if (cntPerPage == null) { 
-			cntPerPage = "5";
+			cntPerPage = "20";
 		}
 		vo = new PagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
 		model.addAttribute("paging", vo);
